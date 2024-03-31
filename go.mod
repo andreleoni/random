05d1be97e4b0f7cfe3f79135018fd7e4f0dc1adf
Andre Leoni <andreluizleoni@gmail.com>
2024-03-31 19:21:42 -0300

    random letters

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/andreleoni/random
+
+go 1.22.1
---
+++ letters.go
@@ -0,0 +1,15 @@
+package random
+
+import "math/rand"
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func Letters(n int) string {
+	b := make([]rune, n)
+
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+
+	return string(b)
+}
